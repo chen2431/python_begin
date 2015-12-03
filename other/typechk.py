31a2ad5356b1454a51ee979482835978433a2ad5
Wanghongchen <chen2431@hotmail.com>
2015-12-03 15:08:18 +0800

    add typechk.py for check data type

---
+++ other/typechk.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+def displayNumType(num):
+	print num, 'is',
+	if isinstance(num, (int, long,float, complex)):
+		print 'a number of type:',type(num),type(num).__name__
+	else:
+		print 'not a number at all!!'
+
+displayNumType(-69)
+displayNumType(9999999999999999999999L)
+displayNumType(98.6)
+displayNumType(-5.2+1.9j)
+displayNumType('xxx')
